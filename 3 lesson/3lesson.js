03f8a8b8ae5ee9c330eca27daec6ff944703332e
marialoseva88 <marialoseva88@gmail.com>
2024-10-15 10:25:20 -0400

    15.10.24

---
+++ 3 lesson/3lesson.js
@@ -0,0 +1,6 @@
+const titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = 1788
+let percentage = 56
+let fullPrice = 2000
+let responsive = true
